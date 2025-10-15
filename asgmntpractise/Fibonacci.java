a540eb9a05745f66510c4afbf225f3c72db576fd
mzameer <mdzameer108@gmail.com>
2025-10-15 21:04:48 +0530

    added few more problems

--- asgmntpractise/Fibonacci.java
+++ asgmntpractise/Fibonacci.java
@@ -2,16 +2,13 @@ package asgmntpractise;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int n = 10;
-        int first = 0, second = 1;
-        for(int i=0; i<=10; i++){
+      //fibonacci
+        int first = 0,second = 1;
+        for (int i=0; i<=10;i++){
             System.out.print(first+" ");
-
             int third = first+second;
             first=second;
             second=third;
-            //added the comments
-
         }
     }
 }
---
+++ asgmntpractise/ReverseString.java
@@ -0,0 +1,15 @@
+package asgmntpractise;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String input = "zameer";
+        char[] charArray = input.toCharArray();
+        for (int i=0; i<charArray.length/2;i++){
+            char temp = charArray[i];
+            charArray[i]=charArray[charArray.length-1-i];
+            charArray[charArray.length-1-i]=temp;
+        }
+        String newStr = new String(charArray);
+        System.out.println("reversed string:"+newStr);
+    }
+}
